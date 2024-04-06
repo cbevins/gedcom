20460b8b05dd150efd36f6e07fd449a87cc351cc
Collin D Bevins <cbevins@montana.com>
2024-04-06 16:36:20 -0600

    Removed _stats and gens[] from Vines.js

--- src/lib/Sylvan/class/Vines.js
+++ src/lib/Sylvan/class/Vines.js
@@ -7,25 +7,16 @@ import { VineNode } from './VineNode.js'
 export class Vines {
     constructor(subject) {
         this._data = {
-            gens: [],           // Array of VineNode arrays by generation
-            maxNodeCount: -1,   // Most nodes in any generation
             map: new Map(),     // Map of Person => Ancestor
             root: null,         // reference to the subject's VineNode instance (root node of Vines)
             subject: subject,
         }
         this._data.root = this._recurse(subject, 0, 1, null)
-        this._stats()
     }
     
     findPerson(person) { return this.map().get(person) }
     
-    gen(idx) { return this._data.gens[idx] }
-    
-    gens() { return this._data.gens }
-    
     map() { return this._data.map }
-
-    maxNodeCount() { return this._data.maxNodeCount }
 
     // Returns array of [<Person>, <VineNode>] pairs ordered by VineNode.seq()
     nodesBySeq() { return Array.from(this.map()).sort((a, b) => { return (a[1].childSeq() - b[1].childSeq()) }) }
@@ -51,21 +42,5 @@ export class Vines {
             this._data.map.set(child, node)
         }
         return node
-    }
-    
-    _stats() {
-        let currGen = -1
-        const nodes = this.nodesBySeq() // array of [Person, VineNode] pairs
-        for (let i=0; i<nodes.length; i++) {
-            const [person, node] = nodes[i]
-            if (node.childGen() > currGen) {
-                this._data.gens.push([])
-                currGen++
-            }
-            node.setPos(this._data.gens[currGen].length)
-            this._data.gens[currGen].push(node)
-        }
-        for (let i=0; i<this.gens().length; i++)
-            this._data.maxNodeCount = Math.max(this._data.maxNodeCount, this.gen(i).length)
     }
 }
