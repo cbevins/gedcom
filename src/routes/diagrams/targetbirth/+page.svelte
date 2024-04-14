e486bc6af4ce1197a4ca3f9790b2d7fa06bb9086
Collin D Bevins <cbevins@montana.com>
2024-04-13 20:30:03 -0600

    Update diagrams/targetbirth/+page.svelte

--- src/routes/diagrams/targetbirth/+page.svelte
+++ src/routes/diagrams/targetbirth/+page.svelte
@@ -3,10 +3,12 @@
     import { getSylvan } from '$lib/Sylvan/js/singletons.js'
     // BE SURE TO DE-REFERENCE subjectNameKey VALUE USING '$subjectNameKey'
     import { subjectNameKey } from '$lib/Sylvan/js/store.js'
-    $: label = getSylvan().people().find($subjectNameKey).label()
+    $: subject = getSylvan().people().find($subjectNameKey)
 
-    $: birth = {min: 1479, max: 2021}
-    $: vb = {xmin: -1000, xmax: 1000, ymin: -1000, ymax: 1000}
+    const scale = 1
+    $: nodes = []
+    $: birth = {min: 9999, max: 0}
+    $: vb = {xmin: scale * -1000, xmax: scale * 1000, ymin: scale * -1000, ymax: scale * 1000}
     $: year = {
         max: 2050,
         min: 1450,
@@ -14,51 +16,85 @@
         tic: {count: 0, years: 50},
         units: {perTic: 1, perYear: 1}
     }
-    $: init()
+    $: init(subject)
 
-    function init() {
+    function init(subject) {
+        // First determine subject's ancestors
+        ancestors(subject, 0, 1, null)
+        // Then we can determine birth year range and the view geometry
+        geometry()
+        // Finally, determine ancestor's positions
+        positions()
+        console.log(nodes.length, 'Ancestors')
+    }
+
+    const drawCircles = true
+    const drawRadials = false
+
+    function ancestors(person, gen, seq, child) {
+        const node = {
+            person: person,         // person's Person instance
+            gen: gen,               // person's generation index (0 for the root person)
+            seq: seq,               // person's ancestral sequence (1 for the root person)
+            mother: null,           // mother's node instance
+            father: null,           // father's node instance
+            child: child,           // descendant's node instance
+            birth: person.birthYear(),
+            label: person.fullName(),
+            deg: 0, x: 0, y: 0      // set by positions()
+        }
+        nodes.push(node)
+        if (person.mother() || person.father()) {
+            if (person.father()) node.father = ancestors(person.father(), gen+1, seq*2, node)
+            if (person.mother()) node.mother = ancestors(person.mother(), gen+1, seq*2+1, node)
+        }
+        return node
+    }
+
+    function geometry() {
         // Set viewBox user units
         vb.width = vb.xmax - vb.xmin
         vb.height = vb.ymax - vb.ymin
-        // First determine min and max birth years
+        // Determine min and max birth years for the ancestors
+        for (let i=0; i<nodes.length; i++) {
+            const yr = nodes[i].birth
+            if (yr) {
+                birth.min = Math.min(birth.min, yr)
+                birth.max = Math.max(birth.max, yr)
+            }
+        }
+        console.log('Birth year range:', birth.min, birth.max)
         // Then adjust target years range
         year.min = year.tic.years * Math.trunc(birth.min / year.tic.years)
         year.max = year.tic.years * Math.trunc(birth.max / year.tic.years) + year.tic.years
         year.span = year.max - year.min
-        console.log(year.min, year.max, year.span)
         year.tic.count = year.span / year.tic.years
         year.units.perYear = vb.width / (2*year.span)
         year.units.perTic = year.tic.years * year.units.perYear
-        console.log('Tic count', year.tic.count, 'un/yr', year.units.perYear, 'un/tic', year.units.perTic)
     }
-    
-    function poly(id) {
-        const gen = idGenIdx(id)        // subject gen === 0
-        const segs =idGenCount(id)
-        const slot = idGenSlot(id)
-        const segDeg = 360 / segs
-        const fromDeg = slot * segDeg
-        const thruDeg = fromDeg + segDeg
-        // outer
-        const p0 = point((gen+1)*adelta, (gen+1)*bdelta, fromDeg)
-        const p1 = point((gen+1)*adelta, (gen+1)*bdelta, thruDeg)
-        // inner
-        const p2 = point(gen*adelta, gen*bdelta, fromDeg)
-        const p3 = point(gen*adelta, gen*bdelta, thruDeg)
-        const points = [p0, p1, p3, p2]
-        let d = `M ${p0[0]} ${p0[1]}`
-            + ` A ${(gen+1)*adelta} ${(gen+1)*bdelta} 0 0 1 ${p1[0]} ${p1[1]}`
-        if (segs > 1) {
-            d += ` L ${p3[0]} ${p3[1]}`
-            d += ` A ${gen*adelta} ${gen*bdelta} 0 0 0 ${p2[0]} ${p2[1]}`
-            d += ` L ${p0[0]} ${p0[1]}`
+
+    function positions() {
+        for (let i=0; i<nodes.length; i++) {
+            const node = nodes[i]
+            node.deg = seqDegrees(node.seq)
+            node.x = seqX(node.seq, node.birth)
+            node.y = seqY(node.seq, node.birth)
         }
-        return d
     }
+
+    // Returns target angle in degrees for Ancestor seq
+    function seqDegrees(seq) {
+        const slot = idGenSlot(seq)
+        const slots = idGenCount(seq)
+        const deg = 360 * slot/ slots + 96
+        return deg
+    }
+    function seqX(seq, yr) { return pointX(yr, seqDegrees(seq)) }
+    function seqY(seq, yr) { return pointY(yr, seqDegrees(seq)) }
     
-    function point(yr, deg, xCenter=0, yCenter=0) {
-        return [pointX(yr, deg, xCenter), pointY(yr, deg, yCenter)]
-    }
+    // function point(yr, deg, xCenter=0, yCenter=0) {
+    //     return [pointX(yr, deg, xCenter), pointY(yr, deg, yCenter)]
+    // }
 
     function pointX(yr, deg, xCenter=0) {
         const rad = (360-deg) * Math.PI / 180
@@ -73,32 +109,51 @@
     }
 </script>
 
-<h3>Ancestral Birth Disc for {label}</h3>
+<h3>Ancestral Birth Disc for {subject.label()}</h3>
 <svg width={vb.width} height={vb.height} viewBox="{vb.xmin} {vb.ymin} {vb.width} {vb.height}">
-    {#each Array(year.tic.count+1) as unused, i}
-        <circle cx={0} cy={0} r={i*year.units.perTic} fill="none" stroke="black" stroke-width="1" />
-        <text x={0-18} y={i * -year.units.perTic + 5}>{year.max - i * year.tic.years}</text>
-        <text x={i * -year.units.perTic - 18} y={0 + 5}>{year.max - i * year.tic.years}</text>
-        <text x={0-18} y={i * year.units.perTic + 5}>{year.max - i * year.tic.years}</text>
-        <text x={i * year.units.perTic-18} y={0 + 5}>{year.max - i * year.tic.years}</text>
-    {/each}
+    <!-- Target disc and axis -->
+    {#if drawCircles}
+        {#each Array(year.tic.count+1) as unused, i}
+            <circle cx={0} cy={0} r={i*year.units.perTic} fill="none" stroke="black" stroke-width="1" />
+            <text x={0-18} y={i * -year.units.perTic + 5}>{year.max - i * year.tic.years}</text>
+            <text x={i * -year.units.perTic - 18} y={0 + 5}>{year.max - i * year.tic.years}</text>
+            <text x={0-18} y={i * year.units.perTic + 5}>{year.max - i * year.tic.years}</text>
+            <text x={i * year.units.perTic-18} y={0 + 5}>{year.max - i * year.tic.years}</text>
+        {/each}
+    {/if}
 
-    {#each Array(32) as unused, i}
-        <line class='axis' x1={0} y1={0} x2={pointX(1500, i*360/32)} y2={pointY(1500, i*360/32)} />
-        <text class='axis' x={pointX(1500, i*360/32)} y={pointY(1500, i*360/32)}>{i*360/32}</text>
+    {#if drawRadials}
+        {#each Array(32) as unused, i}
+            <line class='axis' x1={0} y1={0} x2={pointX(1500, i*360/32)} y2={pointY(1500, i*360/32)} />
+            <text class='axis' x={pointX(1500, i*360/32)} y={pointY(1500, i*360/32)}>{i*360/32}</text>
+        {/each}
+    {/if}
+
+    {#each nodes as node}
+        <circle class="dot" cx={node.x} cy={node.y} r="10" />
+        <text class="axis" x={node.x} y={node.y}>{node.label}</text>
+        {#if node.child && node.birth && node.child.birth}
+            <line class="link" x1={node.x} y1={node.y} x2={node.child.x} y2={node.child.y} />
+        {/if}
     {/each}
 </svg>
 
 <style>
     .axis {
         fill: none;
+        font-size: 72;
         stroke: black;
         stroke-width: 1;
         stroke-linecap: flat;
     }
-    .sep {
-        fill: none;
+    .dot {
+        fill: red;
         stroke: black;
-        stroke-width: 2
+        stroke-width: 1
+    }
+    .link {
+        fill: red;
+        stroke: red;
+        stroke-width: 1
     }
 </style>
