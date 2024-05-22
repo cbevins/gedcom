97ee9b1cc7a7c246b6b25b46a94f7d709cfb45ec
Collin D Bevins <cbevins@montana.com>
2024-05-22 11:33:45 -0600

    Added Settings.svelte

---
+++ src/lib/Sylvan/svelte/Settings.svelte
@@ -0,0 +1,34 @@
+<script>
+    import SylvanSubjectSelector from '$lib/Sylvan/svelte/SylvanSubjectSelector.svelte'
+    import { getSylvan } from '$lib/Sylvan/js/singletons.js'
+
+    export let id = 'settings'
+    export let title = 'Your Title Here'
+
+    $: fullId = id + '-settings'
+    $: target = '#' + fullId
+
+</script>
+
+<div class="container-fluid mt-3" style="position: fixed;">
+    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target={target}>
+        {title}
+    </button>
+</div>
+
+<div class="offcanvas offcanvas-start text-bg-dark" id={fullId}>
+    <div class="offcanvas-header">
+        <h3 class="offcanvas-title">{title}</h3>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"></button>
+    </div>
+    <hr/>
+    <div class="offcanvas-body">
+        <div class="row">
+            <h5>Subject Person</h5>
+            <SylvanSubjectSelector sylvan={getSylvan()}/>
+        </div>
+        <hr/>
+        <slot />
+    </div>
+</div>
+
--- src/routes/diagrams/gxml/+page.svelte
+++ src/routes/diagrams/gxml/+page.svelte
@@ -1,10 +1,9 @@
 <script>
+    import Settings from '$lib/Sylvan/svelte/Settings.svelte'
     import LineageTrainPosterSvg from '$lib/LineageTrainPosterSvg/LineageTrainPosterSvg.svelte'
     import { US_History, US_Migrations, US_Wars, World_History } from '$lib/LineageTrainPosterSvg/timelineData.js'
     import { SheetDefs } from '$lib/LineageTrainPosterSvg/sheetDefs.js'
     import { subjectNameKey } from '$lib/Sylvan/js/store.js'
-    import SylvanSubjectSelector from '$lib/Sylvan/svelte/SylvanSubjectSelector.svelte'
-    import { getSylvan } from '$lib/Sylvan/js/singletons.js'
 
     $: settings = {
         guides: false,
@@ -22,92 +21,61 @@
     const sheetLabels = []
     SheetDefs.forEach((sheet, idx) => {sheetLabels[idx] = sheet.name})
 </script>
-    
-<div class="container-fluid mt-3" style="position: fixed;">
-    <button class="btn btn-primary" type="button"
-        data-bs-toggle="offcanvas" data-bs-target="#train-settings">
-        Settings
-    </button>
-</div>
 
-<div class="offcanvas offcanvas-start text-bg-dark" id="train-settings">
-    <div class="offcanvas-header">
-        <h3 class="offcanvas-title">Settings</h3>
-        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"></button>
+<Settings id="train" title="Settings">
+
+    <div class="row">
+        <h5>Display Width</h5>
+        {#each scaleValues as scaleValue, i}
+            <div class="form-check">
+                <input type="radio" class="form-check-input" id="size{i}"
+                    name="pageSizes"
+                    value={scaleValue}
+                    bind:group={settings.scale} checked>
+                {scaleLabels[i]} inches
+                <label class="form-check-label" for="size{i}"></label>
+            </div>
+        {/each}
     </div>
+
     <hr/>
-    <div class="offcanvas-body">
-        <div class="row">
-            <h5>Root Person</h5>
-            <SylvanSubjectSelector sylvan={getSylvan()}/>
-        </div>
-        <hr/>
-        <div class="row">
-            <h5>Display Width</h5>
-            {#each scaleValues as scaleValue, i}
-                <div class="form-check">
-                    <input type="radio" class="form-check-input" id="size{i}"
-                        name="pageSizes"
-                        value={scaleValue}
-                        bind:group={settings.scale} checked>
-                    {scaleLabels[i]} inches
-                    <label class="form-check-label" for="size{i}"></label>
-                </div>
-            {/each}
-        </div>
-        <hr/>
-        <div class="row">
-            <h5>Rule Guides</h5>
-            {#each guideValues as guideValue, i}
-                <div class="form-check">
-                    <input type="radio" class="form-check-input" id="guideopt{i}"
-                        name="guides"
-                        value={guideValue}
-                        bind:group={settings.guides}>
-                    {guideLabels[i]}
-                    <label class="form-check-label" for="guideopt{i}"></label>
-                </div>
-            {/each}
-        </div>
-        <hr/>
-        <div class="row">
-            <h5>Upper TimeLine</h5>
-            <select class="form-select"
-                bind:value={settings.upperTimeline}>
-                <option value={US_Migrations}>US_Migrations</option>
-                <option value={US_History}>US_History</option>
-                <option value={US_Wars}>US_Wars</option>
-                <option value={World_History}>World_History</option>
-            </select>
-        </div>
-        <hr />
-        <div class="row">
-            <h5>Lower TimeLine</h5>
-            <select class="form-select"
-                bind:value={settings.lowerTimeline}>
-                <option value={US_Migrations}>US_Migrations</option>
-                <option value={US_History}>US_History</option>
-                <option value={US_Wars}>US_Wars</option>
-                <option value={World_History}>World_History</option>
-            </select>
-        </div>
-<!-- 
-        <div class="row">
-            <h5>Display Sheet</h5>
-            {#each sheetValues as sheetValue, i}
-                <div class="form-check">
-                    <input type="radio" class="form-check-input" id="sheetopt{i}"
-                        name="sheet"
-                        value={sheetValue}
-                        bind:group={sheetNumber}>
-                    {sheetLabels[i]}
-                    <label class="form-check-label" for="sheetopt{i}"></label>
-                </div>
-            {/each}
-        </div>
-        <hr/>
-         -->
+    <div class="row">
+        <h5>Rule Guides</h5>
+        {#each guideValues as guideValue, i}
+            <div class="form-check">
+                <input type="radio" class="form-check-input" id="guideopt{i}"
+                    name="guides"
+                    value={guideValue}
+                    bind:group={settings.guides}>
+                {guideLabels[i]}
+                <label class="form-check-label" for="guideopt{i}"></label>
+            </div>
+        {/each}
     </div>
-</div>
 
+    <hr/>
+    <div class="row">
+        <h5>Upper TimeLine</h5>
+        <select class="form-select"
+            bind:value={settings.upperTimeline}>
+            <option value={US_Migrations}>US_Migrations</option>
+            <option value={US_History}>US_History</option>
+            <option value={US_Wars}>US_Wars</option>
+            <option value={World_History}>World_History</option>
+        </select>
+    </div>
+
+    <hr />
+    <div class="row">
+        <h5>Lower TimeLine</h5>
+        <select class="form-select"
+            bind:value={settings.lowerTimeline}>
+            <option value={US_Migrations}>US_Migrations</option>
+            <option value={US_History}>US_History</option>
+            <option value={US_Wars}>US_Wars</option>
+            <option value={World_History}>World_History</option>
+        </select>
+    </div>
+
+</Settings>
 <LineageTrainPosterSvg subjectNameKey={$subjectNameKey} {settings} />
