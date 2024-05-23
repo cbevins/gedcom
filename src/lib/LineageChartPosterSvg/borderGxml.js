bcbb4bc5ce2c355d6e8fa2e7811d99c24b254360
Collin D Bevins <cbevins@montana.com>
2024-05-23 10:37:11 -0600

    Added classic borderGxml.js to LineageChartPosterSvg

--- src/lib/LineageChartPosterSvg/borderGxml.js
+++ src/lib/LineageChartPosterSvg/borderGxml.js
@@ -1,45 +1,39 @@
 /**
  * Returns an array of Gxml JSON objects defining content for the poster's 'border' region.
- * In this case, it draws a border in the style of train tracks.
+ * In this case, it draws a border in the classic style.
  * @param {layout} layout The layout rturned by portraitLayout()
  * @returns An array of Gxml JSON objects
  */
 export function borderGxml(layout) {
     const border = layout.border
-    const bt = border.thickness / 2
-    const off1 = 2 * bt / 10
-    const off2 = 8 * bt / 10
-    const offsets = [off1, off2]
     const els = []
 
-    //  Railroad ties
+    //  Wide black border
     els.push({el: 'rect', id: 'border-rect',
         x: 0,
         y: 0,
         width: border.width,
         height: border.height,
-        fill: 'none',
+        fill: 'black',
         stroke: 'black',
         'stroke-width': border.thickness,
-        'stroke-dasharray': '4 4',
         'stroke-linejoin': "miter",
         fill: 'none',
     })
 
-    // Rails
-    const w = border.width
-    const h = border.height
-    for(let i=0; i<2; i++) {
-        const o = offsets[i]
-        els.push({el: 'rect',
-            x: o,
-            y: o,
-            width: w-2*o,
-            height: h-2*o,
-            fill: 'none',
-            stroke: 'black',
-            'stroke-width': 1
-        })
-    }
+    //  White dashes
+    const offset = border.thickness / 4
+    els.push({el: 'rect', id: 'border-rect',
+        x: 0 + offset,
+        y: 0 + offset,
+        width: border.width - 2 * offset,
+        height: border.height - 2 * offset,
+        fill: 'none',
+        stroke: 'white',
+        'stroke-width': 0.4 * border.thickness,
+        'stroke-dasharray': '25 25',
+        'stroke-linejoin': "miter",
+        fill: 'none',
+    })
     return els
 }
