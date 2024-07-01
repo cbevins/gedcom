eec06e06b552091e03c5ae8393fd777bc8a3d1ba
Collin D Bevins <cbevins@montana.com>
2024-07-01 17:03:00 -0600

    Added dunsel routes/diagrams/pages.svelte

---
+++ src/routes/diagrams/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+
+console.log('RUNNING src/routes/diagrams/page.svelte')
+</script>
+
+<h3>routes/diagrams/+page.svelte</h3>
+<p>
+	This page should not be displayed,
+	Only its child routes/diagrams pages should be accessible.
+</p>
